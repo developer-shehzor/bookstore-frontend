36ab092886d6da5e0212ae7ed0048f8e276f9dbb
ShehzorMemon <Shezymemon0180@gmail.com>
2025-01-09 14:36:53 +0500

    Created react app with vite

---
+++ src/pages/DeleteBook.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DeleteBook = () => {
+  return (
+    <div>
+      dele
+    </div>
+  )
+}
+
+export default DeleteBook
---
+++ src/pages/ShowBook.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShowBook = () => {
+  return (
+    <div>
+      show
+    </div>
+  )
+}
+
+export default ShowBook
